249e5ed56ee0c90e7e390de92bb19900a657d969
chunminggg <651403538@qq.com>
2018-09-04 22:12:11 +0800

    done

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'12345678',
+    password:'19940711Miao.',
     database:'express'
 })
 module.exports = connection
