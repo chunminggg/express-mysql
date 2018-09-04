38bb93cbeadf77a2e849498021c73386f531c757
neo-mac <miaobingliang@neotrident.com>
2018-09-04 08:55:08 +0800

    done

--- database/island.js
+++ database/island.js
@@ -1,16 +1,47 @@
 const con = require('./index')
 // 查找所有的信息
- const island = {
-      findAll(res){
-         let sql = 'SELECT * FROM island'
-         con.query(sql,(error,result)=>{
-             res.send(result)
-         })
-     },
-     async insertItem(item){
-         let sql = `INSERT INTO island (name,description) VALUES (${item.name},${item.description})`
-         let data = await con.query(sql)
-         return data
-     }
- }
- module.exports = island
+const island = {
+    findAll(res) {
+        let sql = 'SELECT * FROM island'
+        con.query(sql, (error, result) => {
+            res.send(result)
+        })
+    },
+    async updateItem(req,res){
+        let item = req.body
+        let sql = `UPDATE island SET name ="${item.name}",description="${item.description} ,WHERE id= ${parseInt(item.id)}"`
+        con.query(sql,(error,data)=>{
+            if (error) {
+                res.send('更新失败')
+            } else {
+                res.send('更新成功')
+            }
+        })
+    },
+    async insertItem(req, res) {
+        let item = req.body
+        let sql = `INSERT INTO island (name,description) VALUES ("${item.name}","${item.description}")`
+        let findSql = 'SELECT name FROM island'
+        con.query(findSql, (error, data) => {
+            let isExist = false
+            data.forEach(obj => {
+                if (obj.name == item.name) {
+                    isExist = true
+                }
+            });
+            if (isExist) {
+                res.send('已有该数据')
+            } else {
+                con.query(sql, (err, result) => {
+                    if (err) {
+                        res.send('保存失败')
+                    } else {
+                        res.send('保存成功')
+                    }
+                })
+            }
+        })
+
+    }
+}
+module.exports = island
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', function (req, res, next) {
 router.get('/island', (req, res, next) => {
     island.findAll(res)
 })
+router.post('/island', (req,res,next)=>{
+  island.insertItem(req,res)
+})
+router.put('/island',(req,res,next)=>{
+  island.updateItem(req,res)
+})
 module.exports = router;
