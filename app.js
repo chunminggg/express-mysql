8e69109824bea01f0e4c6f071265dbdb00571e65
neo-mac <miaobingliang@neotrident.com>
2018-09-06 16:48:59 +0800

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(cors())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
